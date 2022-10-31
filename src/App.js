1f489cc5a6700d483435d00b7346c79874181fb3
GOGU EDUKONDAL <97788201+goguedukondal@users.noreply.github.com>
2022-10-31 15:14:54 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+        <h1>About Us</h1>
+        <p>React is the most popular JavaScript front-end framework in use today. It's used by both established companies and new startups.</p>
+        <p>React was released by Facebook in 2013, and they still use it today for many of their applications. If you're new to React, this article will help introduce you to the basics.Components in React can manage their own state and communicate that state to child components. By "state," we mean the data that populates the web application.</p>
+        
+        <p>This is where React got its start and where you'll find it used most often. React is component-based. An example of a component could be a form or even just a form field or button on a website. In React, you build up complete applications using components like these by nesting them _________</p>
+    </div>
+    
+  );
+}
+
+export default App;
